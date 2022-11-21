610f8da3bc45f3e829609f91e5445daa45342470
Voss <voss@visnovalabs.io>
2022-11-20 19:41:49 -0600

    remove nimbus add lodestar

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,17 +5,17 @@ case "$_DAPPNODE_GLOBAL_CONSENSUS_CLIENT_MAINNET" in
     echo "Using prysm.dnp.dappnode.eth"
     JWT_PATH="/security/prysm/jwtsecret.hex"
     ;;
-"lighthouse.dnp.dappnode.eth")
-    echo "Using lighthouse.dnp.dappnode.eth"
+"lighthouse-gnosis.dnp.dappnode.eth")
+    echo "Using lighthouse-gnosis.dnp.dappnode.eth"
     JWT_PATH="/security/lighthouse/jwtsecret.hex"
     ;;
-"teku.dnp.dappnode.eth")
-    echo "Using teku.dnp.dappnode.eth"
+"teku-gnosis.dnp.dappnode.eth")
+    echo "Using teku-gnosis.dnp.dappnode.eth"
     JWT_PATH="/security/teku/jwtsecret.hex"
     ;;
-"nimbus.dnp.dappnode.eth")
-    echo "Using nimbus.dnp.dappnode.eth"
-    JWT_PATH="/security/nimbus/jwtsecret.hex"
+"lodestar-gnosis.dnp.dappnode.eth")
+    echo "Using lodestar-gnosis.dnp.dappnode.eth"
+    JWT_PATH="/security/lodestar/jwtsecret.hex"
     ;;
 *)
     echo "Using default"
@@ -27,37 +27,6 @@ esac
 JWT=$(cat $JWT_PATH)
 curl -X POST "http://my.dappnode/data-send?key=jwt&data=${JWT}"
 
-#####################
-# Datadir migration #
-#####################
-# UPSTREAM: 2021.08.03
-# DAPPNODE: v0.1.7 to v0.1.8
-# Datadir migration must be done manually according to https://github.com/ledgerwatch/erigon/releases/tag/v2021.08.03
-
-PORT="${P2P_PORT:=30303}"
-
-DATADIR="/home/erigon/.local/share"
-
-if [ -d "$DATADIR/erigon/chaindata" ]; then
-    mv "$DATADIR/erigon/chaindata" "$DATADIR"
-fi
-
-############################
-# Check database migration #
-############################
-# UPSTREAM: v2022.04.01
-# DAPPNODE: v0.1.22 to v0.1.23
-
-## Run for 5 secs to check the logs if we found:
-## [EROR] [06-27|17:36:39.664] Erigon startup err="migrator.VerifyVersion: cannot upgrade major DB version for more than 1 version from 3 to 6, use integration tool if you know what you are doing"
-## We need to re-sync
-
-timeout -s 9 5 erigon --datadir=/home/erigon/.local/share ${EXTRA_OPTs} 2>/tmp/initlog.txt
-if grep -e "migrator.VerifyVersion: cannot upgrade major DB version for more than 1 version from 3 to 6, use integration tool if you know what you are doing" /tmp/initlog.txt; then
-    echo "Cannot upgrade major DB version for more than 1 version from 3 to 6"
-    echo "The database will be deleted as it needs to be resynchronized..."
-    rm /home/erigon/.local/share/chaindata/*
-fi
 
 ##########
 # Erigon #
