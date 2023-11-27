f41db92d2cd2384d3f86e52559a879de38d5a1ba
Voss <voss@visnovalabs.io>
2023-11-27 00:26:22 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -57,6 +57,6 @@ exec erigon --datadir=${DATADIR} \
     --authrpc.addr=0.0.0.0 \
     --authrpc.vhosts=* \
     --torrent.port=42169 \
-    --torrent.upload.rate=8mb \
+    --torrent.upload.rate=16mb \
     --torrent.download.rate=32mb \
     ${EXTRA_OPTS}
