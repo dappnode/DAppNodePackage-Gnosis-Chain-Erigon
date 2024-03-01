be1a0be867337ec4a7bde69f27918f79f7dbfd70
Voss <voss@visnovalabs.io>
2024-03-01 06:01:12 -0600

    finish bump upstream and add beacon backfilling ENV, will do setup wizard later

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,6 @@ FROM thorax/erigon:${UPSTREAM_VERSION}
 
 USER root
 
-COPY /security /security
 COPY entrypoint.sh /usr/local/bin/entrypoint.sh
 RUN chmod u+x /usr/local/bin/entrypoint.sh
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,36 +1,5 @@
 #!/bin/sh
 
-case "$_DAPPNODE_GLOBAL_CONSENSUS_CLIENT_GNOSIS" in
-"gnosis-beacon-chain-prysm.dnp.dappnode.eth")
-    echo "Using gnosis-beacon-chain-prysm.dnp.dappnode.eth"
-    JWT_PATH="/security/prysm/jwtsecret.hex"
-    ;;
-"lighthouse-gnosis.dnp.dappnode.eth")
-    echo "Using lighthouse-gnosis.dnp.dappnode.eth"
-    JWT_PATH="/security/lighthouse/jwtsecret.hex"
-    ;;
-"nimbus-gnosis.dnp.dappnode.eth")
-    echo "Using nimbus-gnosis.dnp.dappnode.eth"
-    JWT_PATH="/security/nimbus/jwtsecret.hex"
-    ;;
-"teku-gnosis.dnp.dappnode.eth")
-    echo "Using teku-gnosis.dnp.dappnode.eth"
-    JWT_PATH="/security/teku/jwtsecret.hex"
-    ;;
-"lodestar-gnosis.dnp.dappnode.eth")
-    echo "Using lodestar-gnosis.dnp.dappnode.eth"
-    JWT_PATH="/security/lodestar/jwtsecret.hex"
-    ;;
-*)
-    echo "Using default"
-    JWT_PATH="/security/default/jwtsecret.hex"
-    ;;
-esac
-
-# Print the jwt to the dappmanager
-JWT=$(cat $JWT_PATH)
-curl -X POST "http://my.dappnode/data-send?key=jwt&data=${JWT}"
-
 DATADIR="/home/erigon-gnosis/.local/share"
 
 ##########
@@ -50,6 +19,7 @@ exec erigon --datadir=${DATADIR} \
     --lightclient.discovery.port=${CAPLIN_P2P_PORT} \
     --lightclient.discovery.tcpport=${CAPLIN_P2P_TCP_PORT} \
     --metrics \
+    --caplin.backfilling=${CL_BACKFILLING} \
     --metrics.addr=0.0.0.0 \
     --metrics.port=6060 \
     --pprof \
